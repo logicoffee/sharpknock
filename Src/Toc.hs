1b4e3e9c97b9e0458a32408cecce5e257008617a
logicoffee <riemann1618@gmail.com>
2019-02-16 16:24:13 +0900

    Success compile

---
+++ Src/Toc.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Src.Toc where
+
+import           Data.Function                   (on)
+import           Data.List                       (groupBy)
+import           Data.Tree                       (Forest, Tree (Node))
+import           Text.Blaze                      ((!))
+import           Text.Blaze.Html.Renderer.String
+import qualified Text.Blaze.Html5                as H
+import qualified Text.Blaze.Html5.Attributes     as A
+import           Text.Blaze.Internal             (MarkupM (Empty))
+import           Text.Pandoc
+import           Text.Pandoc.Walk
+
+
+headerLevel :: Block -> Int
+headerLevel (Header level _ _) = level
+headerLevel _                  = error "not header"
+
+filterHeader :: Int -> Block -> [Block]
+filterHeader n header@(Header level _ _) =
+    [header | level <= n]
+filterHeader _ _ = []
+
+makeHeaderForest :: [Block] -> Forest Block
+makeHeaderForest = map (\(x:xs) -> Node x (makeHeaderForest xs)) . groupBy ((<) `on` headerLevel)
+
+markupHeader :: Tree Block -> H.Html
+markupHeader (Node (Header _ (ident, _, _) inlines) headers)
+    | null headers  = H.li link
+    | otherwise     = H.li $ link <> H.ol (markupHeaders headers)
+        where
+            link = H.a ! A.href (H.toValue ("#" ++ ident)) $
+                renderPandocToHtml (Pandoc nullMeta [Plain inlines])
+markupHeader _ = Empty ()
+
+markupHeaders :: Forest Block -> H.Html
+markupHeaders = mconcat . map markupHeader
+
+renderPandocToHtml :: Pandoc -> H.Html
+renderPandocToHtml pandoc =
+    case runPure (writeHtml5 def pandoc) of
+        Left  _    -> Empty ()
+        Right item -> item
+
+renderToc :: Pandoc -> String
+renderToc = renderHtml . markupHeaders . makeHeaderForest . query (filterHeader 3)
+
--- site.hs
+++ site.hs
@@ -3,8 +3,10 @@
 import           Control.Monad
 import           Hakyll
 import           Hakyll.Web.Sass (sassCompiler)
-import           Skylighting (styleToCss, kate)
+import           Skylighting     (kate, styleToCss)
+import           Src.Toc
 import           Text.Pandoc
+import           Text.Regex.TDFA ((=~))
 
 
 --------------------------------------------------------------------------------
@@ -37,6 +39,7 @@ main = hakyll $ do
         route $ setExtension "html"
         compile $ pandocCompilerWith defaultHakyllReaderOptions wOptions
             >>= saveSnapshot "content"
+            >>= tocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
@@ -56,10 +59,10 @@ main = hakyll $ do
                 >>= relativizeUrls
 
     --------- Categories ------------------------------------------------------
-    tagsRules categories $ \category pattern -> do
+    tagsRules categories $ \category patt -> do
         categoryPaginate <- buildPaginateWith
             (sortRecentFirst >=> return . paginateEvery 10)
-            pattern
+            patt
             (fromCapture (fromGlob ("categories/" ++ category ++ "/*.html")) . show)
         paginateRules categoryPaginate $ \pageNum ptn -> do
             route idRoute
@@ -76,10 +79,10 @@ main = hakyll $ do
                     >>= relativizeUrls
 
     --------- Tags ------------------------------------------------------------
-    tagsRules tags $ \tagName pattern -> do
+    tagsRules tags $ \tagName patt -> do
         tagsPaginate <- buildPaginateWith
             (sortRecentFirst >=> return . paginateEvery 10)
-            pattern
+            patt
             (fromCapture (fromGlob ("tags/" ++ tagName ++ "/*.html")) . show)
         paginateRules tagsPaginate $ \pageNum ptn -> do
             route idRoute
@@ -100,11 +103,11 @@ main = hakyll $ do
         (sortRecentFirst >=> return . paginateEvery 10)
         "posts/**"
         (fromCapture "archives/*.html" . show)
-        
-    paginateRules archivePaginate $ \pageNum pattern -> do
+
+    paginateRules archivePaginate $ \pageNum patt -> do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll pattern
+            posts <- recentFirst =<< loadAll patt
             let ctx =
                     listField "posts" postCtx (return posts) <>
                     paginateContext archivePaginate pageNum  <>
@@ -114,7 +117,7 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/archive.html" ctx
                 >>= loadAndApplyTemplate "templates/default.html" ctx
                 >>= relativizeUrls
-   
+
     create ["sitemap.xml"] $ do
         route idRoute
         compile $ do
@@ -125,7 +128,7 @@ main = hakyll $ do
                     defaultContext
             makeItem ""
                 >>= loadAndApplyTemplate "templates/sitemap.xml" ctx
-    
+
     match "templates/*" $ compile templateCompiler
 
 
@@ -146,3 +149,38 @@ wOptions :: WriterOptions
 wOptions = defaultHakyllWriterOptions
     { writerHTMLMathMethod = MathJax ""
     }
+
+
+-------- Toc -------------------------------------------------------------------
+enabledToc :: Item String -> Compiler Bool
+enabledToc item = do
+    result <- getMetadataField (itemIdentifier item) "toc"
+    case result of
+        Nothing -> return False
+        Just _  -> return True
+
+extractToc :: Item String -> Compiler String
+extractToc = fmap (foldMap renderToc) . readPandoc
+
+tocMarker = "{:toc}" :: String
+
+splitAtTocMarker :: String -> Item String -> Compiler (String, String)
+splitAtTocMarker marker item =
+    let body          = itemBody item
+        (o, l)        = body =~ marker
+        (former, tmp) = splitAt o body
+        (_, latter)   = splitAt l tmp
+    in return (former, latter)
+
+
+
+tocCompiler :: Item String -> Compiler (Item String)
+tocCompiler item = do
+    enabled <- enabledToc item
+    if enabled
+        then do
+            (former, latter) <- splitAtTocMarker tocMarker item
+            toc <- extractToc item
+            makeItem $ former ++ toc ++ latter
+        else return item
+
